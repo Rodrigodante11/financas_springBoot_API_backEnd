878f1d10b7074a8029bf2495cb0629158c0c7e8c
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-06 09:25:10 -0300

    subindo o projeto

---
+++ src/main/java/com/rodrigocompany/financas/FinancasApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigocompany.financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasApplication.class, args);
+	}
+
+}
