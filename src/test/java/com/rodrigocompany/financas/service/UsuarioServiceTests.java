786afaeca76aa896d3bfe5a4d6528ed781b2ff34
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-08 12:11:06 -0300

    usando o mock para criar uma objeto falso de UsuarioRepository

--- src/test/java/com/rodrigocompany/financas/service/UsuarioServiceTests.java
+++ src/test/java/com/rodrigocompany/financas/service/UsuarioServiceTests.java
@@ -3,15 +3,18 @@ package com.rodrigocompany.financas.service;
 import com.rodrigocompany.financas.exception.RegraNegocioException;
 import com.rodrigocompany.financas.model.entity.Usuario;
 import com.rodrigocompany.financas.model.repository.UsuarioRepository;
+import com.rodrigocompany.financas.service.implementation.UsuarioServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith( SpringExtension.class)
 @ActiveProfiles("test") // vai procurar o aplication-test.properties e usar o BD em memoria para teste e nao o oficial
 public class UsuarioServiceTests {
@@ -19,11 +22,22 @@ public class UsuarioServiceTests {
 	public static String EMAIL = "usuario@email.com";
 	public static String NOME = "usuario";
 
-	@Autowired
+	// UsuarioRepository repository; == intancia Original
+	// UsuarioRepository usuarioRepositoryMock = Mockito.mock(UsuarioRepository.class); // instancia fake/mockada
+
 	UsuarioService service;
 
-	@Autowired
+	@MockBean
 	UsuarioRepository repository;
+
+	//@Before() // usado no Junit 5 spring boot -2.2
+	@BeforeEach
+	public void setUp(){
+		// A linha abaixo foi tirada pois o annotation @MockBean faz a mesma coisa
+		//repository= Mockito.mock(UsuarioRepository.class); // criando um instancia Mock do UsuarioRepository
+
+		service = new UsuarioServiceImpl(repository); // falando pro service usar a instancia Mock de UsuarioRepository (Muito importante essa parte se nao eh um Mock
+	}
 
 	//@Test(expected = Test.None.class) Usado no Junit 4 para excessoes  no caso nao espera nenhuma excessao
 	@Test
@@ -32,7 +46,13 @@ public class UsuarioServiceTests {
 		Assertions.assertDoesNotThrow(() -> {
 
 			// cenario
-			repository.deleteAll();
+			//repository.existsByEmail(email);
+			Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
+			// La em UsuarioService implementation no metodo validarEmail() tem um existsByEmail()
+			// entao estou dizendo a ele quando tiver um existsByEmail no metodo usado abaixo validarEmail()
+			// criar um instancia FAKE que retorna "true" para qualquer String
+			// Pois nesse caso eu nao quero testes o existsByEmail()
+			// Quero testar o retorno do validarEmail()
 
 			// acao
 			service.validarEmail(EMAIL);
@@ -44,8 +64,16 @@ public class UsuarioServiceTests {
 		Assertions.assertThrows(RegraNegocioException.class, () -> {
 
 			// cenario
-			Usuario usuario = Usuario.builder().nome(NOME).email(EMAIL).build();
-			repository.save(usuario);
+			// Usuario usuario = Usuario.builder().nome(NOME).email(EMAIL).build();
+			// repository.save(usuario);
+
+			Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
+			// La em UsuarioService implementation no metodo validarEmail() tem um existsByEmail()
+			// entao estou dizendo a ele quando tiver um existsByEmail no metodo usado abaixo validarEmail()
+			// criar um instancia FAKE que retorna "true" para qualquer String
+			// Pois nesse caso eu nao quero testes o existsByEmail()
+			// Quero testar o retorno do validarEmail()
+
 
 			// acao
 			service.validarEmail(EMAIL);
