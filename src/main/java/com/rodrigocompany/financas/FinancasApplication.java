7753554c9bffaf81309268a491c2b96a57479215
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-18 18:18:32 -0300

    adicionando spring secutiry e autenticacao em memoria

--- src/main/java/com/rodrigocompany/financas/FinancasApplication.java
+++ src/main/java/com/rodrigocompany/financas/FinancasApplication.java
@@ -7,14 +7,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc // aceitar requisicao para consumir a API
 public class FinancasApplication implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry){
-		registry.addMapping("/**") //url das API que podem ser consultada
-				.allowedMethods("GET","POST","PUT","DELETE", "OPTIONS"); //
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
 	}
